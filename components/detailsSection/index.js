bc49d01fd121df2a1af52629cc47392a9bfc3607
brulyn <higirobru@gmail.com>
2021-10-11 12:33:35 +0200

    Setting up the Global Layout of the App

---
+++ components/detailsSection/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetailsScreen() {
+    return (
+        <div className='min-h-full w-2/6 bg-gray-100'>
+            <p>Details Screen</p>
+        </div>
+    )
+}
