c61633c5f005a6eddff5375a9d108c1aae62b731
Bruce <higirobru@gmail.com>
2021-10-26 13:19:44 +0200

    Get AllFigures data from API

--- components/mainScreen/index.js
+++ components/mainScreen/index.js
@@ -6,14 +6,40 @@ import OutputView from "./outputView";
 import ReportsView from "./ReportsView";
 import { RatioContext } from "../../contexts/ratioContext";
 import { DataLoadedContext } from "../../contexts/dataLoadedContext";
+import { UserContext } from "../../contexts/userContext";
 
 export default function MainScreen() {
   const { view, setView } = useContext(ViewContext);
+  const { user, setUser } = useContext(UserContext);
   const [ratios, setRatios] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     console.log(view);
+  }, [view]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/allFigures/${user.companyName}/Q12021`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        console.log(Object.keys(response).length);
+        if (response && Object.keys(response).length === 0) {
+        } else {
+          setLoaded(true);
+          setRatios(response);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [view]);
   return (
     <div className="min-h-full flex-1 bg-gray-50 pt-5 pl-5">
