343a4c80a79969c8c22f2e72352c09f4fa8e6d9a
higirobruce <higirobru@gmail.com>
2022-02-21 18:28:04 +0200

    moving user actions on the sidebar

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -30,61 +30,7 @@ export default function Navbar() {
         {/* <div className="hidden md:flex flex-row items-center font-bold text-xl text-gray-600 ">
         Risk Management System
       </div> */}
-
-        <div
-          className="flex flex-row justify-between space-x-2 mr-5 items-center cursor-pointer"
-          onClick={() => {
-            setShowUserActions(!showUserActions);
-          }}
-          onBlur={(event) => {
-            if (!event.currentTarget.contains(event.relatedTarget)) {
-              // do your thing.
-            } else {
-              setShowUserActions(false);
-            }
-          }}
-        >
-          {/* Names */}
-          <div className="text-blue-cvl-900 text-base hover:text-blue-cvl-700 hover:underline">
-            {user.email}
-          </div>
-          <div class="flex-shrink-0 h-12 w-12 rounded-full shadow-sm p-1 hover:scale-105 hover:shadow-md transition ease-in-out duration-200 ">
-            <Image
-              height="150"
-              width="150"
-              class="rounded-full object-contain"
-              src={`/logos/${user.companyName}.jpg`}
-              alt=""
-            />
-          </div>
-        </div>
       </div>
-      {showUserActions && (
-        <span className="absolute top-16 right-6 rounded-md bg-white w-56 shadow-lg">
-          <div className="grid grid-cols-1 justify-between divide-y">
-            <div className="cursor-pointer py-4 px-4 hover:bg-blue-cvl-50 hover:text-blue-cvl-700 transition ease-in-out duration-300">
-              Notifications
-            </div>
-            <div className="cursor-pointer py-4 px-4 hover:bg-blue-cvl-50 hover:text-blue-cvl-700 transition ease-in-out duration-300">
-              Inbox
-            </div>
-
-            <div className="cursor-pointer py-4 px-4 hover:bg-blue-cvl-50 hover:text-blue-cvl-700 transition ease-in-out duration-300">
-              Profile
-            </div>
-            <div
-              onClick={() => {
-                setUser({});
-                window.localStorage.removeItem("user");
-                router.push("/");
-              }}
-              className="cursor-pointer py-4 px-4 hover:bg-blue-cvl-50 hover:text-blue-cvl-700 transition ease-in-out duration-300 "
-            >
-              Logout
-            </div>
-          </div>
-        </span>
-      )}
     </div>
   );
 }
